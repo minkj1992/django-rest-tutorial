8fdf87ec8b50767c2c1d42421c6ab4891f9be949
minkj1992 <minkj1992@gmail.com>
2020-07-04 19:53:48 +0900

    Refactor REST style urls

--- django_rest_tutorials/serialization_tutorial/snippets/urls.py
+++ django_rest_tutorials/serialization_tutorial/snippets/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 urlpatterns = [
     path('snippets/', views.snippet_list),
     path('snippets/<int:pk>/', views.snippet_detail),
 ]
+
+# url에 .json, .html, .api등을 추가하여 타입을 요청할 수 있다.
+urlpatterns = format_suffix_patterns(urlpatterns)
