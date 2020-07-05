af9dd94e05410d15ec0c49cbb4ad283fc2712a31
minkj1992 <minkj1992@gmail.com>
2020-07-05 17:09:35 +0900

    Add self-descriptive principle to hyperlink REST-API

--- django_rest_tutorials/serialization_tutorial/snippets/serializers.py
+++ django_rest_tutorials/serialization_tutorial/snippets/serializers.py
@@ -4,23 +4,25 @@ from django.contrib.auth.models import User
 from snippets.models import Snippet
 
 
-class SnippetSerializer(serializers.ModelSerializer):
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
     """Snippet/JSON... -> Python OrderedDict"""
     
     # not be used for updating model instances when they are deserialized. 
     # We could have also used CharField(read_only=True) here.
     owner = serializers.ReadOnlyField(source='owner.username')
+    # url field to link -> highlight
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
 
     class Meta:
         model = Snippet
-        fields = ('id', 'owner','title', 'code', 'linenos', 'language', 'style',)
+        fields = ('url','id', 'highlight', 'owner','title', 'code','linenos', 'language', 'style',)
 
 
-class UserSerializer(serializers.ModelSerializer):
-    snippets =  serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
 
     class Meta:
         model = User
         # snippets is reverse relationship attribute to auth.User
         # by default when using the ModelSerializer class(not include reverse relationship), so we needed to add an explicit field for it.
-        fields = ['id', 'username', 'snippets']
+        fields = ('id', 'username', 'snippets')
--- django_rest_tutorials/serialization_tutorial/snippets/urls.py
+++ django_rest_tutorials/serialization_tutorial/snippets/urls.py
@@ -3,11 +3,12 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 urlpatterns = [
-    path('snippets/', views.SnippetList.as_view()),
-    path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
-    path('snippets/<int:pk>/highlight/', views.SnippetHighlight.as_view()),
-    path('users/', views.UserList.as_view()),
-    path('users/<int:pk>/', views.UserDetail.as_view()),
+    path('', views.api_root),
+    path('snippets/', views.SnippetList.as_view(), name='snippet-list'),
+    path('snippets/<int:pk>/', views.SnippetDetail.as_view(), name='snippet-detail'),
+    path('snippets/<int:pk>/highlight/', views.SnippetHighlight.as_view(), name='snippet-highlight'),
+    path('users/', views.UserList.as_view(), name='user-list'),
+    path('users/<int:pk>/', views.UserDetail.as_view(), name='user-detail'),
 ]
 
 # url에 .json, .html, .api등을 추가하여 타입을 요청할 수 있다.
