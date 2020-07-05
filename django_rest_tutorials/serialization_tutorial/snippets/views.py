4db9ab3fadea86ea7e91bdde11f5d95f9402e915
minkj1992 <minkj1992@gmail.com>
2020-07-05 16:32:19 +0900

    Add code highlighting logic

--- django_rest_tutorials/serialization_tutorial/snippets/views.py
+++ django_rest_tutorials/serialization_tutorial/snippets/views.py
@@ -1,11 +1,25 @@
 from rest_framework import generics
 from rest_framework import permissions
+from rest_framework import renderers
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+
 from django.contrib.auth.models import User
 
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 from snippets.serializers import UserSerializer
 from snippets.permissions import IsOwnerOrReadOnly
+
+
+# API root entry point
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format),
+    })
 
 
 class SnippetList(generics.ListCreateAPIView):
@@ -35,6 +49,16 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
         permissions.IsAuthenticatedOrReadOnly,
         IsOwnerOrReadOnly,
     )
+
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    rendered_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object() # Returns the object the view is displaying.
+        return Response(snippet.highlighted)
+
 
 class UserList(generics.ListAPIView):
     """
