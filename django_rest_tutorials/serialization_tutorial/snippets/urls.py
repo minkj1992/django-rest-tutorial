1193e3400ffdcc47073857a04800ffd53ce6f103
minkj1992 <minkj1992@gmail.com>
2020-07-05 18:01:32 +0900

    Refactor urls.py with Router and path->url

--- django_rest_tutorials/serialization_tutorial/snippets/urls.py
+++ django_rest_tutorials/serialization_tutorial/snippets/urls.py
@@ -1,38 +1,13 @@
-from django.urls import path
-from rest_framework.urlpatterns import format_suffix_patterns
+from django.conf.urls import include, url
+from rest_framework.routers import DefaultRouter
+
 from snippets import views
-from snippets.views import SnippetViewSet, UserViewSet, api_root
-from rest_framework import renderers
 
-
-snippet_list = SnippetViewSet.as_view({
-    'get': 'list',
-    'post': 'create'
-})
-snippet_detail = SnippetViewSet.as_view({
-    'get': 'retrieve',
-    'put': 'update',
-    'patch': 'partial_update',
-    'delete': 'destroy'
-})
-snippet_highlight = SnippetViewSet.as_view({
-    'get': 'highlight'
-}, renderer_classes=[renderers.StaticHTMLRenderer])
-user_list = UserViewSet.as_view({
-    'get': 'list'
-})
-user_detail = UserViewSet.as_view({
-    'get': 'retrieve'
-})
+# suffix처리기 포함
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet)
+router.register(r'users', views.UserViewSet)
 
 urlpatterns = [
-    path('', api_root),
-    path('snippets/', snippet_list, name='snippet-list'),
-    path('snippets/<int:pk>/', snippet_detail, name='snippet-detail'),
-    path('snippets/<int:pk>/highlight/', snippet_highlight, name='snippet-highlight'),
-    path('users/', user_list, name='user-list'),
-    path('users/<int:pk>/', user_detail, name='user-detail'),
+    url(r'^', include(router.urls)),
 ]
-
-# url에 .json, .html, .api등을 추가하여 타입을 요청할 수 있다.
-urlpatterns = format_suffix_patterns(urlpatterns)
